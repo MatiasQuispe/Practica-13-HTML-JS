5731a4eeb104fec730fc66320c206a3d430516bd
MatiasQuispe <matiasquispe2062@gmail.com>
2022-11-01 14:55:27 -0300

    Practica 13 HTML/JS

---
+++ puntoEquilibrio.js
@@ -0,0 +1,28 @@
+/*
+
+Sin JQuerry
+
+var costosFijos = prompt("Ingrese los costos fijos");
+
+var precioVenta = prompt("Ingrese el precio de venta");
+
+var costosVariables = prompt("Ingrese los costos variables");
+
+var puntoEquilibrio = costosFijos / (precioVenta - costosVariables)
+
+alert("El punto de equilibrio es " + puntoEquilibrio);
+
+var ingreso = prompt("Ingrese el monto de su ingreso");
+
+var gastosNecesarios = ingreso * 0.5;
+
+var gastosOpcionales = ingreso * 0.3;
+
+var ahorroInversion = ingreso * 0.2;
+
+alert("Distribucion de sus ingresos:");
+alert("Gastos necesarios: " + gastosNecesarios);
+alert("Gastos opcionales: " + gastosOpcionales);
+alert("Ahorro e inversión: " + ahorroInversion);
+
+*/
